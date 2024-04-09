b2778ae14408f260d7ef4bc6322f9bf8d8d3bc91
thub2005776 <thub2005776@student.ctu.edu.vn>
2024-04-09 15:36:10 +0700

    modified profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,11 +1,15 @@
 <script setup>
-import { BookCard } from '@/components';
+import { BBookCard } from '@/components';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
 const user = route.meta.user;
-const books = route.meta.books;
 const bbooks = route.meta.bbooks;
+const favorite = route.meta.favorite;
+
+const borrBook = bbooks.filter(f => !f.returned );
+const brrwedBook = bbooks.filter(f => f.returned);
+
 </script>
 
 <template>
@@ -27,77 +31,30 @@ const bbooks = route.meta.bbooks;
             <div>
                 <div class="mb-6">
                     <h1 class="font-bold text-lg mb-6">Sách đang mượn</h1>
-                    <p class="text-center text-gray-500">
+                    <p v-if="borrBook.length == 0" class="text-center text-gray-500">
                         Chưa có sách đang mượn
-                        {{ borrowedBooks }}
                     </p>
-                    <ol class="items-center sm:flex">
-                        <li class="relative mb-6 sm:mb-0">
-                            <div class="flex items-center">
-                                <div
-                                    class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                                    <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                        <path
-                                            d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-                                    </svg>
-                                </div>
-                                <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-                            </div>
-                            <div class="mt-3 sm:pe-8">
-                                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.0.0
-                                </h3>
-                                <time
-                                    class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released
-                                    on December 2, 2021</time>
-                                <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with
-                                    dozens of
-                                    web components and interactive elements.</p>
-                            </div>
-                        </li>
+                    <ol v-if="borrBook.length > 0" class="items-center sm:flex">
+                        <BBookCard v-for="book in borrBook" :key="book.bid" :book="book" />
                     </ol>
                 </div>
                 <div class="mb-6">
                     <h1 class="font-bold text-lg mb-6">Sách đã mượn</h1>
-                    <p class="text-center text-gray-500">
+                    <p v-if="brrwedBook.length == 0" class="text-center text-gray-500">
                         Chưa có sách đã mượn
-                        {{ borrowedBooks }}
                     </p>
-                    <ol class="items-center sm:flex">
-                        <li class="relative mb-6 sm:mb-0">
-                            <div class="flex items-center">
-                                <div
-                                    class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                                    <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                        <path
-                                            d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-                                    </svg>
-                                </div>
-                                <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-                            </div>
-                            <div class="mt-3 sm:pe-8">
-                                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.0.0
-                                </h3>
-                                <time
-                                    class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released
-                                    on December 2, 2021</time>
-                                <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with
-                                    dozens of
-                                    web components and interactive elements.</p>
-                            </div>
-                        </li>
-
+                    <ol v-if="brrwedBook.length > 0" class="items-center sm:flex">
+                        <BBookCard v-for="book in brrwedBook" :key="book.bid" :book="book" />
                     </ol>
                 </div>
             </div>
         </div>
         <div>
             <h1 class="font-bold text-lg my-2">Danh sách yêu thích</h1>
-            <p v-if="user.favorite.length == 0" class="text-center text-gray-500">
+            <p v-if="favorite.length == 0" class="text-center text-gray-500">
                 Chưa có sách trong danh sách yêu thích
             </p>
-            <BookCard v-if="user.favorite.length > 0" v-for="book in user.favorite"></BookCard>
+            <BBookCard v-if="favorite.length > 0" v-for="book in favorite" :key="book.bid" :book="book"/>
         </div>
     </div>
 </template>
